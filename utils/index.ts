84479bcb259d58ee5b95d7eedeae458f0c448af2
tuannl1987 <tuannl1987.nlt@gmail.com>
2024-03-07 16:17:08 +0700

    change url

--- utils/index.ts
+++ utils/index.ts
@@ -9,7 +9,7 @@ export async function fetchCars(filters : FilterProps) {
 		'X-RapidAPI-Host': '' + process.env.xRapidApiHost
 	};
 
-    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=${manufacturer}&year=${year}&make=${model}&limit=${limit}&fuel_type=${fuel}`, {
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
         headers: headers,
     });
 
